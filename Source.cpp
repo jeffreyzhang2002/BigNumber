cdf70da14ca181f754c999fd4ee17f6c65ba333c
Jeffrey Zhang <jeffreyzhang2002@gmail.com>
2021-06-21 20:18:25 -0400

    basic testing and experimentation in c++. No progress today

--- Source.cpp
+++ Source.cpp
@@ -6,6 +6,31 @@ import BigNumber;
 
 int main()
 {
-	Number n = { "12432" };
+//	std::cout << "a: " << sizeof(unsigned char) << "\n";
+//	std::cout << "b: " << sizeof(unsigned short) << "\n";
+//	std::cout << "c: " << sizeof(unsigned int) << "\n";
+//	std::cout << "d: " << sizeof(unsigned long) << "\n";
+//	std::cout << "e: " << sizeof(unsigned long long) << "\n";
+//	std::cout << "f: " << sizeof(unsigned bool) << "\n";
+//
+//	std::cout << "a: " << (int)(((unsigned char) -1)) << "\n";
+//	std::cout << "b: " << ((unsigned short)-1) << "\n";
+//	std::cout << "c: " << ((unsigned int)-1) << "\n";
+//	std::cout << "d: " << ((unsigned long)-1) << "\n";
+//	std::cout << "e: " << ((unsigned long long)-1) << "\n";
+//	std::cout << "f: " << ((unsigned bool)-1) << "\n";
+//
+///*
+//a: 255
+//b : 65535
+//c : 4294967295
+//d : 4294967295
+//e : 18446744073709551615
+//f : 4294967295
+//*/
+
+	Number n {"test"};
+	Number<unsigned int> a {"test"};
+
 	std::cin.get();
 }
